7a4db89ee8e2205876314261264717b863077fa8
jaime escobar <jaimeescobarmallen6@gmail.com>
2025-02-28 08:56:38 -0600

    FIRST COMMIT_APPCINE

---
+++ flutter_application_1/lib/src/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primaryColor = color(0xFF006BF3);
+}
---
+++ flutter_application_1/lib/src/core/constants/app_text_styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+
+  static const labelStyle = TextStyle(
+    color: Colors.black, 
+    fontWeight: FontWeight.w700,
+    fontSize: 40, 
+  );
+  static const unselectedLabelStyle = TextStyle(
+    color: Colors.black87, 
+    fontWeight: FontWeight.w600, 
+    fontSize:24, 
+  );
+  static const movieNameTextStyle = TextStyle(
+    fontSize: 45, 
+    fontWeight: FontWeight.bold, 
+
+  );
+
+  static const movieDetails = TextStyle(height:1.5, fontSize: 20);
+
+  static const bookButtonTextStyle = TextStyle(
+      color: Colors.white, 
+      fontSize: 20, 
+      fontWeight: FontWeight.w500,
+      letterSpacing: 1.1);
+  
+  static const movieDescriptionStyle = 
+      Te
+
+
+}
